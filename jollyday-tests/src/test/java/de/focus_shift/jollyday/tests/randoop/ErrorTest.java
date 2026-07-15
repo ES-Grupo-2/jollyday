b8184837171759d5170c27e545d314e651924d03
luizacarvalhoo <luizaodcarvalho@gmail.com>
2026-07-15 19:49:10 -0300

    [test] randoop generated tests

---
+++ jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/randoop/ErrorTest.java
@@ -0,0 +1,10 @@
+package de.focus_shift.jollyday.tests.randoop;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ Error0Test.class })
+public class ErrorTest {
+}
+
